56cc63617e90c5af7ad99b461619e52d264a6577
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-10-27 11:23:10 +1100

    MOODLE-1294: Refactores behat contexts into traits.

---
+++ tests/behat/context_definition/assertions.php
@@ -0,0 +1,133 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_rollover
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @codingStandardsIgnoreFile
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Exception\ExpectationException;
+
+require_once(__DIR__ . '/../../../../../lib/behat/behat_base.php');
+
+/**
+ * @package     local_rollover
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait local_rollover_behat_context_definition_for_assertions {
+    /**
+     * @Then /^I (should(?: not)?) see the following source options: +\# local_rollover$/
+     */
+    public function iShouldSeeTheFollowingSourceOptions($shouldornot, TableNode $courses) {
+        $courses = $courses->getColumn(0);
+        $shouldnot = ($shouldornot == 'should') ? '' : 'not_';
+        $context = "behat_general::assert_element_{$shouldnot}contains_text";
+
+        foreach ($courses as $course) {
+            $this->execute(
+                $context,
+                [$course, '#local_rollover-your_units', 'css_element']
+            );
+        }
+    }
+
+    /**
+     * @Then /^I should see the checkbox "([^"]*)" ((?:un)?selected)( and disabled)? +\# local_rollover$/
+     */
+    public function iShouldSeeTheCheckbox($checkbox, $selectedornot, $disabled = null) {
+        $selected = ($selectedornot == 'selected');
+        $disabled = ($disabled != null);
+        $element = $this->find_field($checkbox);
+
+        if ($element->isChecked() != $selected) {
+            throw new ExpectationException('"' . $checkbox . '" should be ' . $selectedornot, $this->getSession());
+        }
+
+        if ($disabled && !$element->getAttribute('disabled')) {
+            throw new ExpectationException('"' . $checkbox . '" should be disabled.', $this->getSession());
+        }
+    }
+
+    /**
+     * @Then /^I should (see|not see) the following "([^"]*)" +\# local_rollover$/
+     */
+    public function iShouldSeeTheFollowing($seeornot, $texts) {
+        $assertion = $seeornot == 'see' ? 'assert_page_contains_text' : 'assert_page_not_contains_text';
+        $texts = explode(';', $texts);
+
+        foreach ($texts as $text) {
+            if (empty($text)) {
+                continue;
+            }
+            $this->execute("behat_general::{$assertion}", [$text]);
+        }
+    }
+
+    /**
+     * @Then /^the activities options should be: +\# local_rollover$/
+     */
+    public function theActivitiesOptionsShouldBe(TableNode $foundactivities) {
+        foreach ($foundactivities->getColumnsHash() as $activity) {
+            $name = $activity['activity'];
+            $expectselected = ($activity['selected'] == 'yes');
+            $expectmodifiable = ($activity['modifiable'] == 'yes');
+
+            if ($expectmodifiable) {
+                $ischecked = $this->is_modifiable_activity_selected($name);
+            } else {
+                $ischecked = $this->is_not_modifiable_activity_selected($name);
+            }
+
+            if ($ischecked != $expectselected) {
+                throw new ExpectationException('"' . $name . '" should be selected: ' . $activity['selected'],
+                                               $this->getSession());
+            }
+        }
+    }
+
+    private function is_modifiable_activity_selected($name) {
+        $element = $this->find_field($name);
+        $ischecked = $element->isChecked();
+        return $ischecked;
+    }
+
+    private function is_not_modifiable_activity_selected($name) {
+        /** @var NodeElement[] $foundactivities */
+        $foundactivities = $this->find_all('css', '.grouped_settings.activity_level');
+
+        foreach ($foundactivities as $foundactivity) {
+            $label = $foundactivity->find('css', '.fstaticlabel');
+            $label = trim($label->getText());
+
+            if ($label == $name) {
+                $img = $foundactivity->find('css', 'img.smallicon');
+                $foundselected = strtolower($img->getAttribute('title'));
+                return ($foundselected == 'yes');
+            }
+        }
+
+        throw new ExpectationException('Static "' . $name . '" not found. Maybe it is modifiable.', $this->getSession());
+    }
+}
---
+++ tests/behat/context_definition/data_generation.php
@@ -0,0 +1,153 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_rollover
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @codingStandardsIgnoreFile
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+use Behat\Gherkin\Node\TableNode;
+use local_rollover\admin\rollover_settings;
+use local_rollover\admin\settings_controller;
+use local_rollover\dml\activity_rule_db;
+use local_rollover\test\generator;
+
+require_once(__DIR__ . '/../../../../../lib/behat/behat_base.php');
+
+/**
+ * @package     local_rollover
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait local_rollover_behat_context_definition_for_data_generation {
+    /** @var generator */
+    private $generator = null;
+
+    /**
+     * @return generator
+     */
+    public function generator() {
+        if (is_null($this->generator)) {
+            $this->generator = new generator();
+        }
+        return $this->generator;
+    }
+
+    /**
+     * @Given /^there is a course with shortname "([^"]*)" +\# local_rollover$/
+     */
+    public function thereIsACourseWithShortname($shortname) {
+        $this->generator()->create_course_by_shortname($shortname);
+    }
+
+    /**
+     * @Given /^the course "([^"]*)" has an? ([a-z]+) "([^"]*)" +\# local_rollover$/
+     */
+    public function theCourseHasAnActivity($shortname, $activity, $name) {
+        $this->generator()->create_activity($shortname, $activity, $name);
+    }
+
+    /**
+     * @Given /^I (can|cannot) modify the following courses: +\# local_rollover$/
+     */
+    public function iCanModifyTheFollowingCourses($canornot, TableNode $courses) {
+        $can = ($canornot == 'can');
+        $courses = $courses->getColumn(0);
+
+        foreach ($courses as $course) {
+            $this->generator()->create_course_by_shortname($course);
+            if ($can) {
+                $this->generator()->enrol_editing_teacher($this->myusername, $course);
+            }
+        }
+    }
+
+    /**
+     * @Given /^the course "([^"]*)" has a student "([^"]*)" +\# local_rollover$/
+     */
+    public function theCourseHasAStudent($course, $user) {
+        $this->generator()->enrol_student($user, $course);
+    }
+
+    /**
+     * @Given /^the default rollover settings do not include anything by default +\# local_rollover$/
+     */
+    public function theDefaultRolloverSettingsDoNotIncludeAnythingByDefault() {
+        $options = array_keys(rollover_settings::get_rollover_options_defaults());
+        foreach ($options as $option) {
+            set_config("option_{$option}", 0, 'local_rollover');
+        }
+    }
+
+    /**
+     * @Given /^the course "([^"]*)" has an HTML block "([^"]*)" +\# local_rollover$/
+     */
+    public function theCourseHasAnHTMLBlock($course, $text) {
+        $this->generator()->create_html_block($course, $text);
+    }
+
+    /**
+     * @Given /^the past instances RegEx is set to "([^"]*)" +\# local_rollover$/
+     */
+    public function thePastInstancesRegExIsSetTo($regex) {
+        set_config(settings_controller::SETTING_PAST_INSTANCES_REGEX, $regex, 'local_rollover');
+    }
+
+    /**
+     * @Given /^the following activity rollover rules? exists?: +\# local_rollover$/
+     */
+    public function theFollowingActivityRolloverRulesExist(TableNode $rules) {
+        global $DB;
+        $dml = new activity_rule_db();
+
+        foreach ($rules->getColumnsHash() as $rule) {
+            $rule = (object)$rule;
+
+            switch ($rule->rule) {
+                case 'forbid':
+                    $rule->rule = activity_rule_db::RULE_FORBID;
+                    break;
+                case 'enforce':
+                    $rule->rule = activity_rule_db::RULE_ENFORCE;
+                    break;
+                case 'not default':
+                    $rule->rule = activity_rule_db::RULE_NOT_DEFAULT;
+                    break;
+                default:
+                    throw new moodle_exception("Invalid rule: {$rule->rule}");
+            }
+
+            $rule->activity = strtolower($rule->activity);
+            if (empty($rule->activity)) {
+                $rule->moduleid = null;
+            } else {
+                if ($rule->activity == 'assignment') {
+                    $rule->activity = 'assign';
+                }
+                $rule->moduleid = $DB->get_field('modules', 'id', ['name' => $rule->activity], MUST_EXIST);
+            }
+            unset($rule->activity);
+
+            $dml->save($rule);
+        }
+    }
+}
