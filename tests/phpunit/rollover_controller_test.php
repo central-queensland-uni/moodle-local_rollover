b3809925f4634b084c8ac01116317b74029830be
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-11-06 16:07:26 +1100

    MOODLE-1331: Removed dependency for data and duplicated tests (available as feature).

--- tests/phpunit/rollover_controller_test.php
+++ tests/phpunit/rollover_controller_test.php
@@ -136,44 +136,6 @@ class local_rollover_rollover_controller_test extends rollover_testcase {
         self::assertSame($expected, $courses);
     }
 
-    public function test_it_respects_the_include_role_assignments_option() {
-        $this->markTestSkipped('Test/Feature not yet implemented.');
-
-        self::resetAfterTest(true);
-
-        $sourcecourse = $this->generator()->create_course_by_shortname('rollover-from');
-        $destinationcourse = $this->generator()->create_course_by_shortname('rollover-into');
-        $this->generator()->create_activity('rollover-from', 'assignment', 'Full Rollover Assignment');
-
-        $_GET[rollover_parameters::PARAM_DESTINATION_COURSE_ID] = $sourcecourse->id;
-        $controller = new rollover_controller();
-        $controller->rollover($sourcecourse->id, $destinationcourse->id, ['activities' => 1]);
-
-        course_modinfo::clear_instance_cache($destinationcourse);
-        $info = get_fast_modinfo($destinationcourse);
-        $cm = array_values($info->get_cms())[0];
-        self::assertSame('Full Rollover Assignment', $cm->name);
-    }
-
-    public function test_it_respects_the_not_include_role_assignments_option() {
-        $this->markTestSkipped('Test/Feature not yet implemented.');
-
-        self::resetAfterTest(true);
-
-        $sourcecourse = $this->generator()->create_course_by_shortname('rollover-from');
-        $destinationcourse = $this->generator()->create_course_by_shortname('rollover-into');
-        $this->generator()->create_activity('rollover-from', 'assignment', 'Full Rollover Assignment');
-
-        $_GET[rollover_parameters::PARAM_DESTINATION_COURSE_ID] = $sourcecourse->id;
-        $controller = new rollover_controller();
-        $controller->rollover($sourcecourse->id, $destinationcourse->id, ['options' => ['activities' => 0]]);
-
-        course_modinfo::clear_instance_cache($destinationcourse);
-        $info = get_fast_modinfo($destinationcourse);
-        $cms = $info->get_cms();
-        self::assertCount(0, $cms);
-    }
-
     public function test_it_runs_as_admin() {
         // Is it really needed to run as user 2 (admin)?
         // Let's figure out when we the specific capabilities.
